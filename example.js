755c09d4232237d26f3b81469bd3064e94a42b97
Huseyin Elmas <hsynlms47@gmail.com>
2021-05-23 02:26:26 +0300

    refactor: cleanup

--- example.js
+++ example.js
@@ -1,16 +1,13 @@
 'use strict'
 
-// get required modules
 const fastify = require('fastify')()
 const sequelizeFastify = require('./src/index')
 const chalk = require('chalk')
 
-// defaults
 const defaults = {
   port: 3000
 }
 
-// register the plugin
 fastify.register(
   sequelizeFastify,
   {
@@ -35,22 +32,18 @@ fastify.register(
       // first connection as test
       await fastify.db.authenticate()
 
-      // log
       console.log(
         chalk.green('Database connection is successfully established.')
       )
 
-      // close the server
       fastify.close()
     } catch (err) {
-      // log the error
       console.log(
         chalk.red(`Connection could not established: ${err}`)
       )
     }
   })
 
-// initialize the fastify server
 fastify.listen(defaults.port, () => {
   console.log(
     chalk.bgYellow(
--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 'use strict'
 
-// get required modules
 const fastifyPlugin = require('fastify-plugin')
 const Sequelize = require('sequelize')
 
-// plugin defaults
 const defaults = {
   instance: 'sequelize',
   sequelizeOptions: {}
 }
 
-// declare sequelize plugin for fastify
 function sequelizePlugin (fastify, opts, done) {
-  // combine defaults with provided options
   const options = Object.assign({}, defaults, opts)
-
-  // create a sequelize instance
   const sequelize = new Sequelize(options.sequelizeOptions)
 
-  // create a decorator as instance name if its provided
   if (typeof options.instance === 'string' && options.instance) {
     fastify.decorate(options.instance, sequelize)
   }
@@ -26,19 +19,15 @@ function sequelizePlugin (fastify, opts, done) {
   // close sequelize database connection before shutdown
   // 'onClose' is triggered when fastify.close() is invoked to stop the server
   fastify.addHook('onClose', async (instance) => {
-    // close the connection
     await sequelize.close()
 
-    // done
     // eslint-disable-next-line no-useless-return
     return
   })
 
-  // done
   done()
 }
 
-// export the plugin
 module.exports = fastifyPlugin(
   sequelizePlugin,
   {

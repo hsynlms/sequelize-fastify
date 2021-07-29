2e12933e56130bdc9739df929e47c0b60a639532
Huseyin Elmas <hsynlms47@gmail.com>
2021-07-29 19:33:17 +0300

    chore: test case

--- test.js
+++ test.js
@@ -1,9 +1,39 @@
 'use strict'
 
+const fastify = require('fastify')()
+const sequelizeFastify = require('./src/index')
+const chalk = require('chalk')
+
 // test cases
 
 // TODO:
 // eslint-disable-next-line
-test('', done => {
-  done()
+test('database connection', done => {
+  fastify.register(
+    sequelizeFastify,
+    {
+      instance: 'db',
+      sequelizeOptions: {
+        dialect: 'mysql',
+        host: 'localhost',
+        user: 'root',
+        database: 'sequelize',
+        password: 'root',
+        port: 3306
+      }
+    }
+  )
+    .ready(async () => {
+      try {
+        await fastify.db.authenticate()
+
+        console.log(
+          chalk.green('Database connection is successfully established.')
+        )
+
+        done()
+      } catch (err) {
+        done(`Connection could not established: ${err}`)
+      }
+    })
 })

d0f3547fb3d87aaf0cb27d28da446191c449ffb3
Arniu Tseng <arniu2006@163.com>
2022-03-02 21:27:01 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,26 @@
 import { FastifyPluginCallback } from "fastify";
 import { Sequelize, Options } from "sequelize";
 
-declare module "fastify" {
-  interface FastifyInstance {
-    sequelize: Sequelize;
-  }
-}
+export type Sequelize = Sequelize;
 
 export interface SequelizePluginOptions {
   /**
    * @default "sequelize"
+   *
+   *
+   * @example
+   *
+   * In order to have typing for the fastify instance, you should follow the example:
+   *
+   * ```
+   * import { Sequelize } from "sequelize-fastify";
+   *
+   * declare module "fastify" {
+   *   interface FastifyInstance {
+   *     [instance]: Sequelize;
+   *   }
+   * }
+   * ```
    */
   instance?: string;
 
